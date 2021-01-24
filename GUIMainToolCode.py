0cc2554689141a56a6dea5bc1ae44efbf4e8cd9c
Okkar Min <omin001@e.ntu.edu.sg>
2021-01-24 13:07:48 +0800

    🔧 Update GUIMainToolCode to use ErrorAnalysisLayout and Logic

--- GUIMainToolCode.py
+++ GUIMainToolCode.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 from GUI.Layout import get_layout
 from GUI.Logic.VerilogCodeGeneratorTabLogic import VerilogCodeGeneratorTabLogic
+from GUI.Logic.ErrorAnalysisTabLogic import ErrorAnalysisTabLogic
 
 sg.theme('SystemDefault')
 sg.SetOptions(font=('Helvetica', 15))
@@ -13,13 +14,12 @@ window = sg.Window(
 while True:
     event, values = window.read()
 
-    print(event)
-
     # If user closed window with X or if user clicked "Exit" button then exit
     if event == sg.WIN_CLOSED or event == 'Exit':
         break
 
     # Reacts to user inputs in respective tabs
     VerilogCodeGeneratorTabLogic(window, event, values)
+    ErrorAnalysisTabLogic(window, event, values)
 
 window.close()
